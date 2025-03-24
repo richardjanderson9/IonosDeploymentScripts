a3d6c9aadc24744276cb9aff27095f53ab733229
Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>
2025-03-24 17:19:50 +0000

    Add files via upload

---
+++ InitialSetup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# InitialSetup.sh
+
+# Create ServiceSetup.service file
+cat <<EOF | sudo tee /etc/systemd/system/ServiceSetup.service
+[Unit]
+Description=ServiceSetup Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/tmp/rj-project/scripts/ServiceSetup.sh
+#RemainAfterExit=yes # Remove this line
+StandardOutput=journal
+StandardError=journal
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Create CustomSetup.service file
+cat <<EOF | sudo tee /etc/systemd/system/CustomSetup.service
+[Unit]
+Description=CustomSetup Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/tmp/rj-project/scripts/CustomSetup.sh
+#RemainAfterExit=yes  # Remove this line
+StandardOutput=journal
+StandardError=journal
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Make the scripts executable
+sudo chmod +x /tmp/rj-project/scripts/ServiceSetup.sh
+sudo chmod +x /tmp/rj-project/scripts/CustomSetup.sh
+
+# Reload systemd
+sudo systemctl daemon-reload
+
+# Enable the services
+sudo systemctl enable ServiceSetup.service
+sudo systemctl enable CustomSetup.service
+
+# Start the services
+sudo systemctl start ServiceSetup.service
+sudo systemctl start CustomSetup.service
---
+++ ServiceSetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# /tmp/rj-project/scripts/ServiceSetup.sh
+
+current_time=$(date +"%d-%m-%y @ %H:%M:%S")
+echo "$current_time" >> /tmp/rj-project/scripts/ServiceSetup.txt
